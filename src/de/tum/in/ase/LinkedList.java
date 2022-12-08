3d9f69f3943b5351f42b52a2c711d5d8078accf3
MetehanOner <a.metehanoner@gmail.com>
2022-12-08 23:23:17 +0100

    Init

--- src/de/tum/in/ase/LinkedList.java
+++ src/de/tum/in/ase/LinkedList.java
@@ -1,9 +1,83 @@
 package de.tum.in.ase;
 
 // TODO: make it implement the interface MyList
-public class LinkedList<T> {
+public class LinkedList<T> implements MyList<T> {
     // TODO: add attributes
+    private ListNode<T> first;
+    private ListNode<T> last;
+
     // TODO: add a constructor
+    public LinkedList() {
+
+    }
     // TODO: add getters and setters
+
+    public ListNode<T> getFirst() {
+        return first;
+    }
+
+    public ListNode<T> getLast() {
+        return last;
+    }
+
+    public void setFirst(ListNode<T> first) {
+        this.first = first;
+    }
+
+    public void setLast(ListNode<T> last) {
+        this.last = last;
+    }
+
+
     // TODO: implement interface methods
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public void add(Object o) {
+
+    }
+
+    @Override
+    public void remove(Object o) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, Object element) {
+
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
 }
--- src/de/tum/in/ase/ListNode.java
+++ src/de/tum/in/ase/ListNode.java
@@ -2,6 +2,46 @@ package de.tum.in.ase;
 
 public class ListNode<T> {
     // TODO: add attributes
+    private T value;
+    private ListNode<T> previous;
+    private ListNode<T> next;
+
     // TODO: add 3 constructors as in the problem statement
+    public ListNode() {
+
+    }
+
+    public ListNode(T value) {
+
+    }
+
+    public ListNode(T value, ListNode<T> previous, ListNode<T> next) {
+
+    }
+
     // TODO: add getters and setters
+
+    public ListNode<T> getNext() {
+        return next;
+    }
+
+    public ListNode<T> getPrevious() {
+        return previous;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public void setNext(ListNode<T> next) {
+        this.next = next;
+    }
+
+    public void setPrevious(ListNode<T> previous) {
+        this.previous = previous;
+    }
 }
